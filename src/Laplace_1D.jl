f688ea86ea2f202e3580562fd7a76b93373c7946
Anna <lyfenko2006@mail.ru>
2026-03-30 10:48:11 +0300

    Добавлены функции для валидации апроксимации исходной функции

--- src/Laplace_1D.jl
+++ src/Laplace_1D.jl
@@ -1,11 +1,14 @@
 module Laplace_1D
 include("Func_Laplace_1D.jl")
-using .Func_Laplace_1D
+include("Func_validation.jl")
+using .Func_Laplace_1D, .Func_validation
+using Distances
 
 N = 5
 u = x -> sin(3x) * cos(4x)
 f = x -> -25 * sin(3x) * cos(4x) - 24 * sin(4x) * cos(3x)
 A, b = create_task(u(0), u(1), f, N)
 y = linear_solv(A, b)
+print(error_Euclidian(y, u, N), "   ", error_Chebyshev(y, u, N))
 
 end # module Laplace_1D
