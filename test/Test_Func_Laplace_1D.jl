793760e7e2138b70c741b34dbdfa55d3e79822fd
Anna <lyfenko2006@mail.ru>
2026-05-02 13:04:57 +0300

    Обновлены зависимости в проекте

--- test/Test_Func_Laplace_1D.jl
+++ test/Test_Func_Laplace_1D.jl
@@ -1,8 +1,7 @@
 module Test_Func_Laplace_1D
-include(Func_Laplace_1D.jl)
+include(joinpath(@__DIR__, "../src/Func_Laplace_1D.jl"))
 using .Func_Laplace_1D
-using Test
+using Test, LinearAlgebra
 @test linear_solv(Tridiagonal([-1, 2], [3, 4, 3], [1, 2]), [1, -3, 5]) ≈ [1, -2, 3]
-
 
 end # module Test_Func_Laplace_1D
